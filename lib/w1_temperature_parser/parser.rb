454deac6692db639064b84a270f0f7c402b1296a
Oliver Noack <oliver.noack@zweitag.de>
2016-05-21 11:44:42 +0200

    Handle error codes and initial value of sensor and retry

--- lib/w1_temperature_parser/parser.rb
+++ lib/w1_temperature_parser/parser.rb
@@ -1,16 +1,34 @@
 module W1TemperatureParser
   class Parser
+    ERROR_VALUES = [127687, 85000]
+    MAXIMUM_TRIES = 1000
+
     attr_reader :file
 
     def initialize(file)
       @file = file
     end
 
-    def call
+    def raw_sensor_value
       lines = File.readlines(file)
       match_result = lines.join.match('t=(\d*)')
-      sensor_value = match_result.values_at(1).first
+      match_result.values_at(1).first
+    end
+
+    def call
+      sensor_value = nil
+      measurements = 0
+      while sensor_value.nil? || ERROR_VALUES.include?(sensor_value.to_i)
+        raise maximum_tries_error_message if measurements > MAXIMUM_TRIES
+        sensor_value = raw_sensor_value
+        measurements += 1
+      end
       Float(sensor_value)/1000
+    end
+
+    private
+    def maximum_tries_error_message
+      "Could not get a valid measurement after #{MAXIMUM_TRIES} attempts, is the sensor configured correctly?"
     end
   end
 end
--- spec/lib/w1_temperature_parser/parser_spec.rb
+++ spec/lib/w1_temperature_parser/parser_spec.rb
@@ -1,11 +1,34 @@
 require 'spec_helper'
 
 describe W1TemperatureParser::Parser do
+  let(:input_file) { File.join(fixture_path, 'valid_sample') }
   subject { described_class.new(input_file) }
+
   context 'valid input file' do
-    let(:input_file) { File.join(fixture_path, 'valid_sample') }
     it 'works' do
       expect(subject.call).to eql 22.875
     end
   end
+
+  context 'invalid input file' do
+    let(:input_file) { File.join(fixture_path, 'invalid_sample') }
+    it 'works' do
+      expect{subject.call}.to raise_error "Could not get a valid measurement after #{W1TemperatureParser::Parser::MAXIMUM_TRIES} attempts, is the sensor configured correctly?"
+    end
+  end
+
+  context 'skips error values' do
+    it 'returns first correct result' do
+      return_values = W1TemperatureParser::Parser::ERROR_VALUES + [23000]
+      allow(subject).to receive(:raw_sensor_value).and_return(*return_values)
+      expect(subject.call).to eql 23.000
+    end
+
+    it 'raises an error if there is no correct value after a maximum amount of attempts' do
+      return_values = [W1TemperatureParser::Parser::ERROR_VALUES.first] * W1TemperatureParser::Parser::MAXIMUM_TRIES
+      allow(subject).to receive(:raw_sensor_value).and_return(*return_values)
+      expect{subject.call}.to raise_error "Could not get a valid measurement after #{W1TemperatureParser::Parser::MAXIMUM_TRIES} attempts, is the sensor configured correctly?"
+    end
+  end
+
 end
